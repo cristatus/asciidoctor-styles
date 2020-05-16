6f0d91aa38c4f67510cda4155d9843dbe1f99648
Amit Mendapara <mendapara.amit@gmail.com>
2020-05-16 19:15:45 +0530

    Add highlight.js support

--- scripts/convert-adoc.js
+++ scripts/convert-adoc.js
@@ -1,7 +1,18 @@
 const path = require("path")
 const asciidoctor = require("@asciidoctor/core")()
+const highlightJsExt = require("asciidoctor-highlight.js")
 
 const { baseDir, buildDir, adocDir, processFiles } = require("./common")
+
+// register highlight.js extension
+highlightJsExt.register(asciidoctor.Extensions)
+
+// highlight.js extension is not adding hljs class to code tag
+asciidoctor.Extensions.register(function () {
+  this.postprocessor(function () {
+    this.process((doc, output) => output.replace(/class="highlight"/g, 'class="hljs highlight"'))
+  })
+})
 
 function convert(file, { standalone = true, attributes = {} } = {}) {
   console.log(`Converting ${path.relative(baseDir, file)}`)
@@ -15,6 +26,7 @@ function convert(file, { standalone = true, attributes = {} } = {}) {
       linkcss: true,
       stylesheet: "../css/asciidoctor-next.css",
       imagesdir: "../adoc/images",
+      "source-highlighter": "highlightjs-ext",
     },
   })
 }
